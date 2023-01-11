67868c0cd8e9135d913475cd746533bcdc662a9b
AMF <work@afdesk.com>
2023-01-11 18:06:32 +0600

    update rpm repository via dist-folder

--- deploy-rpm.sh
+++ deploy-rpm.sh
@@ -5,19 +5,18 @@ function create_rpm_repo () {
         rpm_path=rpm/releases/${version}/x86_64
 
         mkdir -p $rpm_path
-        cp *64bit.rpm ${rpm_path}/${RPM_EL}
+        cp "dist/"*64bit.rpm ${rpm_path}/
 
-        createrepo_c -u https://github.com/aquasecurity/trivy/releases/download/v0.36.1/ --update $rpm_path
+        TRIVY_VERSION=$(find dist/ -type f -name "*64bit.rpm" -printf "%f\n" | head -n1 | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')
+        echo "Create RPM release for Trivy v$TRIVY_VERSION"
 
-        rm ${rpm_path}/${RPM_EL}/*64bit.rpm
+        createrepo_c -u https://github.com/aquasecurity/trivy/releases/download/ --location-prefix="v"$TRIVY_VERSION --update $rpm_path
+
+        rm ${rpm_path}/*64bit.rpm
 }
-
-wget https://github.com/aquasecurity/trivy/releases/download/v0.36.1/trivy_0.36.1_Linux-64bit.rpm
 
 VERSIONS=(5 6 7 8 9)
 for version in ${VERSIONS[@]}; do
         echo "Processing RHEL/CentOS $version..."
         create_rpm_repo $version
 done
-
-rm *64bit.rpm
