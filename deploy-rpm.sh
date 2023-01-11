ddbbdcc1de62b96db60dfb3ec0c90a0d40a31a71
AMF <work@afdesk.com>
2023-01-11 18:13:24 +0600

    update script

--- deploy-rpm.sh
+++ deploy-rpm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+TRIVY_VERSION=$(find dist/ -type f -name "*64bit.rpm" -printf "%f\n" | head -n1 | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')
+echo "Create RPM release for Trivy v$TRIVY_VERSION"
+
 function create_rpm_repo () {
         version=$1
         rpm_path=rpm/releases/${version}/x86_64
 
         mkdir -p $rpm_path
         cp "dist/"*64bit.rpm ${rpm_path}/
-
-        TRIVY_VERSION=$(find dist/ -type f -name "*64bit.rpm" -printf "%f\n" | head -n1 | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')
-        echo "Create RPM release for Trivy v$TRIVY_VERSION"
 
         createrepo_c -u https://github.com/aquasecurity/trivy/releases/download/ --location-prefix="v"$TRIVY_VERSION --update $rpm_path
 
